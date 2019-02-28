6728d6dac4810a31e15f655f2ea6ee80eafd7c13
BlueStew <jonathan.wisser@mymail.champlain.edu>
2019-02-27 19:35:43 -0500

    updated ssh script agian

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -3,11 +3,12 @@
 #Creates a new ssh user called $1
 #Adds a public key to that users authorized keys file
 #Removes roots abilty to ssh in
- useradd test1
-mkdir /home/test1/.ssh
-cp /home/jonathan/sys265-scripts/linux/public-keys/sys265.pub /home/test1/.ssh/authorized_keys
-chmod 700 /home/test1/.ssh
-chmod 600 /home/test1/.ssh/authorized_keys
-chown -R test1:test1  /home/test1/.ssh
+#!/bin/bash
+useradd $1
+mkdir /home/$1/.ssh
+cp /home/jonathan/sys265-scripts/linux/public-keys/sys265.pub /home/$1/.ssh/authorized_keys
+chmod 700 /home/$1/.ssh
+chmod 600 /home/$1/.ssh/authorized_keys
+chown -R $1:$1  /home/$1/.ssh
 
 echo "testing 123"
